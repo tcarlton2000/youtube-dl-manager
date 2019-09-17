8744022bc3cd3c1ae32ae54db0fb2331ea4ed109
Thomas Carlton <tcarlton2000@gmail.com>
2019-09-16 21:55:00 -0500

    Different ERROR and COMPLETED boxes (#26)

--- app/client/components/Download/Download.js
+++ app/client/components/Download/Download.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Segment, Progress, Grid } from 'semantic-ui-react';
+import { Segment, Progress, Grid, Label } from 'semantic-ui-react';
+
+const DOWNLOAD_IN_PROGRESS = 'In Progress';
+const DOWNLOAD_COMPLETED = 'Completed';
+const DOWNLOAD_ERROR = 'Error';
 
 export const Download = ({
   name,
@@ -13,34 +17,21 @@ export const Download = ({
   return (
     <Segment>
       <Grid className="compact">
-        <Grid.Row columns={1}>
-          <Grid.Column>
-            <h4>{name}</h4>
-            <p></p>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row column={1}>
-          <Grid.Column>
-            <Progress
-              percent={Math.floor(percent)}
-              indicating
-              progress
-              autoSuccess
-              margin="0px"
-            />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row columns={3}>
-          <Grid.Column>
-            <p>{size}</p>
-          </Grid.Column>
-          <Grid.Column align="center">
-            <p>{speed}</p>
-          </Grid.Column>
-          <Grid.Column align="right">
-            <p>{timeRemaining}</p>
-          </Grid.Column>
-        </Grid.Row>
+        <DownloadNameAndStatus name={name} status={status} />
+        {status === DOWNLOAD_IN_PROGRESS ? (
+          <DownloadStatusBar percent={percent} />
+        ) : (
+          ''
+        )}
+        {status === DOWNLOAD_IN_PROGRESS ? (
+          <DownloadStatistics
+            size={size}
+            speed={speed}
+            timeRemaining={timeRemaining}
+          />
+        ) : (
+          ''
+        )}
       </Grid>
     </Segment>
   );
@@ -50,6 +41,77 @@ Download.propTypes = {
   name: PropTypes.string,
   status: PropTypes.string,
   percent: PropTypes.number,
+  size: PropTypes.string,
+  speed: PropTypes.string,
+  timeRemaining: PropTypes.string,
+};
+
+const DownloadNameAndStatus = ({ name, status }) => {
+  return (
+    <Grid.Row columns={2}>
+      <Grid.Column width={14}>
+        <h4>{name}</h4>
+        <p />
+      </Grid.Column>
+      <Grid.Column width={2} align="right">
+        {status === DOWNLOAD_ERROR ? <Label color="red">ERROR</Label> : ''}
+        {status === DOWNLOAD_IN_PROGRESS ? (
+          <Label color="yellow">IN PROGRESS</Label>
+        ) : (
+          ''
+        )}
+        {status === DOWNLOAD_COMPLETED ? (
+          <Label color="green">COMPLETED</Label>
+        ) : (
+          ''
+        )}
+      </Grid.Column>
+    </Grid.Row>
+  );
+};
+
+DownloadNameAndStatus.propTypes = {
+  name: PropTypes.string,
+  status: PropTypes.string,
+};
+
+const DownloadStatusBar = ({ percent }) => {
+  return (
+    <Grid.Row column={1}>
+      <Grid.Column>
+        <Progress
+          percent={Math.floor(percent)}
+          indicating
+          progress
+          autoSuccess
+          margin="0px"
+        />
+      </Grid.Column>
+    </Grid.Row>
+  );
+};
+
+DownloadStatusBar.propTypes = {
+  percent: PropTypes.number,
+};
+
+const DownloadStatistics = ({ size, speed, timeRemaining }) => {
+  return (
+    <Grid.Row columns={3}>
+      <Grid.Column>
+        <p>{size}</p>
+      </Grid.Column>
+      <Grid.Column align="center">
+        <p>{speed}</p>
+      </Grid.Column>
+      <Grid.Column align="right">
+        <p>{timeRemaining}</p>
+      </Grid.Column>
+    </Grid.Row>
+  );
+};
+
+DownloadStatistics.propTypes = {
   size: PropTypes.string,
   speed: PropTypes.string,
   timeRemaining: PropTypes.string,
--- app/client/components/Download/Download.stories.js
+++ app/client/components/Download/Download.stories.js
@@ -6,7 +6,7 @@ import Download from '../Download';
 storiesOf('Download', module).add('default', () => (
   <Download
     name={text('name', 'Name')}
-    status={text('status', 'Status')}
+    status={text('status', 'In Progress')}
     percent={number('percent', 50.0)}
     size={text('size', '34.5MiB')}
     speed={text('speed', '2.45MiB/s')}
