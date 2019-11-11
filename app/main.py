c6dc9418cf47550e08e9ddfa41eae3160851c726
Thomas Carlton <tcarlton2000@gmail.com>
2019-11-10 20:56:52 -0600

    Improve JS Performance, gzip bundle (#47)

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_compress import Compress
+
+compress = Compress()
 
 
 def create_app(database_uri, debug=False):
     app = Flask(__name__)
+    compress.init_app(app)
     app.debug = debug
     app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
     db = SQLAlchemy(app)
