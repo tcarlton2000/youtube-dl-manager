d1202a9cf0671708e8be9273affb5a4bfb7a19c3
Thomas Carlton <tcarlton2000@gmail.com>
2020-01-12 15:08:41 -0600

    Status parsing doesn't work when size is ambiguous (#55)

--- test/test_log_parsing.py
+++ test/test_log_parsing.py
@@ -30,6 +30,31 @@ def test_stat_parsing(log, field, value):
 
 
 @pytest.mark.parametrize(
+    "log,field,value",
+    [
+        ("34.7% of ~102.16MiB at 144.30KiB/s ETA 07:53", "percent", 34.7),
+        ("34.7% of ~102.16MiB at 144.30KiB/s ETA 07:53", "size", "~102.16MiB"),
+        ("34.7% of ~102.16MiB at 144.30KiB/s ETA 07:53", "speed", "144.30KiB/s"),
+        ("34.7% of ~102.16MiB at 144.30KiB/s ETA 07:53", "time_remaining", "07:53"),
+        (
+            "34.7% of ~102.16MiB at 144.30KiB/s ETA 01:07:53",
+            "time_remaining",
+            "01:07:53",
+        ),
+    ],
+)
+def test_ambiguous_stat_parsing(log, field, value):
+    # GIVEN
+    file = create_new_file(db)
+
+    # WHEN
+    file.add_to_log(log)
+
+    # THEN
+    assert getattr(file, field) == value
+
+
+@pytest.mark.parametrize(
     "log,name",
     [
         ("[download] Destination: New Video.mp4", "New Video.mp4"),
